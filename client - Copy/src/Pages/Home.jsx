2137d43ecb667212c4b57a3a80390fb48eee3aa9
mouryasujit <mouryasujit04@gmail.com>
2023-03-16 19:41:44 +0530

    adding framer motion animation

--- client - Copy/src/Pages/Home.jsx
+++ client - Copy/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const [Posts, setposts] = useState();
@@ -28,28 +29,37 @@ const Home = () => {
   };
 
   return (
-    <>
-      <div className="home">
-        <div className="posts">
-          {Posts?.map((post) => (
-            <div className="post" key={post.id}>
-              <div className="img">
-                <img src={`./upload/${post.img}`} alt="" />
-              </div>
-              <div className="content">
-                <Link className="link" to={`/post/${post.id}`}>
-                  <h1>{post.title}</h1>
-                </Link>
-                <p>{getText(post.desc).slice(0, 120) + "..."}</p>
-                <button onClick={() => navigate(`/post/${post.id}`)}>
-                  Read More
-                </button>
-              </div>
+    <div className="home">
+      <div className="posts">
+        {Posts?.map((post) => (
+          <motion.div
+            className="post"
+            key={post.id}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false, amount: 0.5 }}
+            transition={{ type: "spring", duration: 2 }}
+            variants={{
+              hidden: { opacity: 0 },
+              visible: { opacity: 1 },
+            }}
+          >
+            <div className="img">
+              <img src={`./upload/${post.img}`} alt="" />
             </div>
-          ))}
-        </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc).slice(0, 120) + "..."}</p>
+              <button onClick={() => navigate(`/post/${post.id}`)}>
+                Read More
+              </button>
+            </div>
+          </motion.div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
